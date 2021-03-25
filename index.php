0765762dd274420179b8a4f9a070423c609140de
Brad Runnels <runnels.brad@gmail.com>
2021-03-25 13:10:41 -0400

    inject container instance of SalsifyCredential into ChannelGroper constructor

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Data\SalsifyCredential;
 use App\Services\ChannelGroper;
 
-$container = (require_once __DIR__.'/config/bootstrap.php')->build();
-
-$channelGroper = $container->get(ChannelGroper::class);
+$container = (require_once __DIR__ .'/config/bootstrap.php')->build();
+$channelGroper = new ChannelGroper(
+    $container->get(SalsifyCredential::class)
+);
 $channelGroper->initiateChannelRun();
 $dataStream = $channelGroper->getChannelData();
+
