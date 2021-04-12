7c0b0044bf45f374bda566151669f079978d7af8
Brad Runnels <runnels.brad@gmail.com>
2021-04-12 12:55:01 -0400

    fetch App\Services\ChannelGroper instance from container

--- index.php
+++ index.php
@@ -4,9 +4,7 @@ use App\Data\SalsifyCredential;
 use App\Services\ChannelGroper;
 
 $container = (require_once __DIR__ .'/config/bootstrap.php')->build();
-$channelGroper = new ChannelGroper(
-    $container->get(SalsifyCredential::class)
-);
+
+$channelGroper = $container->get(ChannelGroper::class);
 $channelGroper->initiateChannelRun();
 $dataStream = $channelGroper->getChannelData();
-
