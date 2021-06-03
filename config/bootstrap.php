39a8f53b974f32aa3a7203630028920a2efad23a
Brad Runnels <runnels.brad@gmail.com>
2021-06-03 16:41:41 -0400

    add DB and Salsify credential defs to container

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -3,12 +3,14 @@
 use DI\ContainerBuilder;
 use App\Data\SalsifyCredential;
 use Psr\Container\ContainerInterface;
+use PDO;
 
 require_once __DIR__.'/../vendor/autoload.php';
 
 $containerBuilder = new ContainerBuilder();
 $containerBuilder->addDefinitions([
     'settings' => (require __DIR__.'/settings.php'),
+
     SalsifyCredential::class => function (ContainerInterface $c) {
         $settings = $c->get('settings');
 
@@ -17,6 +19,20 @@ $containerBuilder->addDefinitions([
             $settings['orgId'],
             $settings['channelId'],
         );
+    },
+
+    PDO::class => function (ContainerInterface $c) {
+        $settings = $c->get('settings')['db'];
+        
+        $host = $settings['host'];
+        $dbname = $settings['database'];
+        $username = $settings['username'];
+        $password = $settings['password'];
+        $charset = $settings['charset'];
+        $flags = $settings['flags'];
+        $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
+        
+        return new PDO($dsn, $username, $password, $flags);
     }
 ]);
 
